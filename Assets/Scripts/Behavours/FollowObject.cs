06451d87dde25a3f38e16199054654d5169c700f
DavidZwit <davidzwitser@gmail.com>
2016-11-30 23:55:18 +0100

    Added the basics of the movement to test

---
+++ Assets/Scripts/Behavours/FollowObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    [SerializeField]
+    float stoppingPower = 10;
+
+    [SerializeField]
+    GameObject target;
+
+    public bool follow = true;
+
+	void Update ()
+    {
+        if (follow == true)
+        {
+            Vector3 distance = new Vector3(
+                target.transform.position.x - gameObject.transform.position.x,
+                target.transform.position.y - gameObject.transform.position.y,
+                0
+             );
+
+            gameObject.transform.position += distance / stoppingPower;
+        }
+    }
+}
---
+++ Assets/Scripts/Behavours/Inputs/ChangePosToTouchPos.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePosToTouchPos : MonoBehaviour {
+
+    Camera camera;
+
+    void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector2 touchPos = camera.ScreenToWorldPoint(Input.GetTouch(0).position);
+        gameObject.transform.position = touchPos;
+    }
+}
