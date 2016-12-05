66ae62baf74988faa24617d6b174cd974b967ae1
nathan-san <18964@ma-web.nl>
2016-12-05 12:33:03 +0100

    Added dash function

--- Assets/Scripts/Behavours/FollowObject.cs
+++ Assets/Scripts/Behavours/FollowObject.cs
@@ -15,6 +15,7 @@ public class FollowObject : MonoBehaviour {
     {
         if (follow == true)
         {
+            
             Vector3 distance = new Vector3(
                 target.transform.position.x - gameObject.transform.position.x,
                 target.transform.position.y - gameObject.transform.position.y,
@@ -24,4 +25,5 @@ public class FollowObject : MonoBehaviour {
             gameObject.transform.position += distance / stoppingPower;
         }
     }
+   
 }
