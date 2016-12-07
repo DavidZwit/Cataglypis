2fa1833d1212bbcc070a2527b591a9281e982e61
nathan-san <18964@ma-web.nl>
2016-12-07 12:27:02 +0100

    Made canvas.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
-    [SerializeField]
     private int lives;
-
     [SerializeField]
     private int maxLives = 3;
-
     [SerializeField]
     private DisplayHealth display;
+
 	void Start () {
         lives = maxLives;
-	}
-	
-	void Update () {
+        display.UpdateDisplay(lives);
 	}
 }
